c8ee62ee2f1be459377f5b8aecb29eb51a936d9e
wppriestley <wppriestley@gmail.com>
2015-10-19 15:31:04 -0400

    Finishing chapter 7 exercises

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,6 +11,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar"}
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
 
   test "valid signup information" do
@@ -22,13 +24,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                     password_confirmation: "foobar"}
     end
     assert_template 'users/show'
-  end
-
-  test "welcome messsage appears for new users" do
-    get signup_path
-      post_via_redirect users_path, user: { name: "Mike Guy",
-                                            email: "mike@guy.com",
-                                            password: "foobar",
-                                            password_confirmation: "foobar"}
+    assert_not flash.empty?
   end
 end
