a7d8193425f51ef369f95f4a5d0ea1eefb8f5458
wppriestley <wppriestley@gmail.com>
2015-10-19 20:31:12 -0400

    Finish log in/log out

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -24,6 +24,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                     password_confirmation: "foobar"}
     end
     assert_template 'users/show'
-    assert_not flash.empty?
+    assert is_logged_in?
   end
 end
